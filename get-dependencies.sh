f7f9ced8521c741991eae3855905b82efc83b025
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-25 22:13:36 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,6 +4,8 @@ set -eu
 
 ARCH=$(uname -m)
 
+pacman -Syu --noconfirm webp-pixbuf-loader
+
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
