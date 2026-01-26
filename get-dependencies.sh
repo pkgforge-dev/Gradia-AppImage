4aaa2b79e59f2ee910e21319f28db62307f4ba05
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-26 02:02:08 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,23 +13,6 @@ get-debloated-pkgs --add-common --prefer-nano
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 
-echo '
-*** Begin Patch
-*** Update File: gradia/backend/ocr.py
-@@
-     def __init__(self, window=None):
-         self.tesseract_cmd = ocr_tesseract_cmd
-         self.original_tessdata_dir = ocr_original_tessdata
--        self.user_tessdata_dir = os.path.expanduser(f"~/.var/app/{app_id}/data/tessdata")
-+        # Use XDG_DATA_HOME for per-user tessdata (default to ~/.local/share)
-+        xdg_data_home = os.getenv('XDG_DATA_HOME', os.path.expanduser('~/.local/share'))
-+        self.user_tessdata_dir = os.path.join(xdg_data_home, app_id, 'tessdata')
-         self.window = window
- 
-         pytesseract.pytesseract.tesseract_cmd = self.tesseract_cmd
-*** End Patch
-' >> ./gradia_ocr.patch
-
 wget --retry-connrefused --tries=30 https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=gradia -O ./PKGBUILD
-sed 's/770b874fed71ec84a6a190b6e931be9189f512b4433eaef8996dca32644472d7/d88fc3a8c08be1832a9d08262ac1b7a76f9c8879064e11bf48fb061b9b066c53/g' ./PKGBUILD
+sed 's/770b874fed71ec84a6a190b6e931be9189f512b4433eaef8996dca32644472d7/9e4bde8a8343f20f69b87e107dae04c1fc6cb09b430872f354fda7fea376b06e/g' ./PKGBUILD
 make-aur-package
