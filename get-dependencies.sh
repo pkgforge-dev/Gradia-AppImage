49c6dddbc5e6496e9c64e7e236ee18ab36376e0d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-26 03:04:23 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,7 +14,7 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=gradia -O ./PKGBUILD
 wget --retry-connrefused --tries=30 https://aur.archlinux.org/cgit/aur.git/plain/gradia_ocr.patch?h=gradia -O ./gradia_ocr.patch
-sed -i 's|Docr_original_tessdata_dir=/usr/share/tessdata|Docr_original_tessdata_dir=$XDG_DATA_HOME/tessdata|g' ./PKGBUILD
+sed -i 's|Docr_original_tessdata_dir=/usr/share/tessdata|Docr_original_tessdata_dir="${XDG_DATA_HOME:-$HOME/.local/share}"/tessdata|g' ./PKGBUILD
 sed -i 's|self.user_tessdata_dir = os.path.expanduser(f"~/.local/var/app/{app_id}/data/tessdata")|self.user_tessdata_dir = os.path.join(os.environ.get("XDG_DATA_HOME", os.path.expanduser("~/.local/share")), "tessdata")|' ./gradia_ocr.patch
 sed -i 's|770b874fed71ec84a6a190b6e931be9189f512b4433eaef8996dca32644472d7|50111f370140fa69773ac66520fdb3d33bc1bc093704bffc1447a0634e74c9df|g' ./PKGBUILD
 make-aur-package
