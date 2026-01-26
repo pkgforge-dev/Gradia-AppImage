f23b885ea9333a6390a6680a47d4622435533080
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-01-26 02:24:44 +0100

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,5 +14,6 @@ echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 
 wget --retry-connrefused --tries=30 https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=gradia -O ./PKGBUILD
+# For OCR patch pass
 sed -i 's/770b874fed71ec84a6a190b6e931be9189f512b4433eaef8996dca32644472d7/9663c58a7cf811470962133bc534446cdca8954432db543a5fa05aa44e381116/g' ./PKGBUILD
 make-aur-package
