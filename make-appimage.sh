4a585c5c6e9c6944739927f9312471ce8805273d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-04-13 10:32:11 +0200

    Replace self-updater.bg.hook with self-updater.hook

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q gradia | awk '{print $2; exit}')
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook"
+export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/be.alexandervanhee.gradia.svg
 export DESKTOP=/usr/share/applications/be.alexandervanhee.gradia.desktop
