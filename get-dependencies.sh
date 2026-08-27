9183d80975daedde75b627b29d50bd14afe11f7d
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2026-08-27 23:54:55 +0200

    gradia is now in official arch repos

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -4,12 +4,8 @@ set -eu
 
 ARCH=$(uname -m)
 
-pacman -Syu --noconfirm webp-pixbuf-loader python-pandas
+pacman -Syu --noconfirm webp-pixbuf-loader python-pandas gradia
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
-
-echo "Installing package dependencies..."
-echo "---------------------------------------------------------------"
-make-aur-package gradia
